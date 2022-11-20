e4e249c581fda34f3c1f464f63920f92c20de3c2
Li-Yu, Yang <tony830802@gmail.com>
2022-11-20 17:59:18 +0800

    add basicAuth

---
+++ src/main/java/com/lyyang/test/testgrpc/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.lyyang.test.testgrpc;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.grpc.CallCredentials;
+import net.devh.boot.grpc.client.inject.StubTransformer;
+import net.devh.boot.grpc.client.security.CallCredentialsHelper;
+
+/**
+ * The security configuration for the client. In this case we assume that we use the same passwords for all stubs. If
+ * you need per stub credentials you can delete the grpcCredentials and define a {@link StubTransformer} yourself.
+ *
+ * @author Daniel Theuke (daniel.theuke@heuboe.de)
+ * @see CallCredentialsHelper
+ */
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfiguration {
+
+    @Value("${auth.username}")
+    private String username;
+
+    @Bean
+        // Create credentials for username + password.
+    CallCredentials grpcCredentials() {
+        return CallCredentialsHelper.basicAuth(this.username, this.username + "Password");
+    }
+
+}
