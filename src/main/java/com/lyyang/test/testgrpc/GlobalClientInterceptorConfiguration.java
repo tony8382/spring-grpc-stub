c53e1f4688975ab13e4ae359bb21840219cade5b
Li-Yu, Yang <tony830802@gmail.com>
2022-11-20 16:50:03 +0800

    add log

---
+++ src/main/java/com/lyyang/test/testgrpc/GlobalClientInterceptorConfiguration.java
@@ -0,0 +1,16 @@
+package com.lyyang.test.testgrpc;
+
+import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+public class GlobalClientInterceptorConfiguration {
+
+    @GrpcGlobalClientInterceptor
+    LogGrpcInterceptor logClientInterceptor() {
+        return new LogGrpcInterceptor();
+    }
+
+}
